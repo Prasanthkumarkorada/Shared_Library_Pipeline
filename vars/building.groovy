1e918b5dfb6e2dee8afaf96062b8d36e35c644eb
Prasanthkumarkorada <37070068+Prasanthkumarkorada@users.noreply.github.com>
2019-09-26 17:47:18 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -2,26 +2,3 @@ def call()
 {
   sh 'mvn clean install'
 } 
-
-/* #!/usr/bin/env groovy
-/*import com.bt.java.BuildConfig */
-def call(def body = [:]) 
-{
-    config = BuildConfig.resolve(body)
-    stage('Package')
-    {
-        withMaven(maven: "${mymaven}")
-        {
-            script
-            {
-                try
-                {
-                    sh "mvn clean install -DskipTests -DskipITs"
-                }catch (err)
-                {
-                    echo 'Error during packaging of REPO ' "${config.GitURL}"
-                }
-            }
-        }
-    }
-} */
